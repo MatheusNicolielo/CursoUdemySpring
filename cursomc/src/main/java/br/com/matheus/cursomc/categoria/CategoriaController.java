ef3128dcf9cd9101a14f5ce563ba46c3671189b2
Matheus MN. Nicolielo <mnicolielo@atakone.com.br>
2019-09-30 15:42:35 -0300

    First Commit

---
+++ cursomc/src/main/java/br/com/matheus/cursomc/categoria/CategoriaController.java
@@ -0,0 +1,12 @@
+package br.com.matheus.cursomc.categoria;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/categorias")
+public class CategoriaController {
+	
+
+	
+}
